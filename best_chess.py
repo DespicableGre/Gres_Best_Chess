bfb53e5a747bfb52ce0c52187d4ea1cc0fc61470
Gre <98617173+DespicableGre@users.noreply.github.com>
2024-11-26 22:13:53 -0600

    Updated best_chess.py
    Got farther than I expected I would lowkey

--- best_chess.py
+++ best_chess.py
@@ -1,14 +1,16 @@
 import re
 class piece:
-    def __init__(self, ICON, SIDE, VALUE):
+    def __init__(self, ICON, SIDE, VALUE, NAME):
         self.icon = ICON
         self.side = SIDE
         self.value = VALUE
+        self.fullName = NAME
     icon = str
     side = bool
     value = int
+    fullName = str
 
-# For testing
+# For testing.      chr(COLUMN), ROW        a1 -> h8
 def Index2Coord(row_column):
     if row_column[0] > 7 or row_column[0] < 0: return "Bruh, Index2Coord(*row_column), 0 ≤ row ≤ 7"
     if row_column[1] > 7 or row_column[1] < 0: return "Bruh2, Index2Coord(row_*column), 0 ≤ column ≤ 7"
@@ -16,26 +18,46 @@ def Index2Coord(row_column):
 
 def Coord2Index(row_column):
     if len(row_column) != 2: return "Bruh, Coord2Index(*row_column*), must be 2 characters"
-    r = ord(row_column[0]) - 97
-    c = int(row_column[1]) - 1
+    c = ord(row_column[0]) - 97 # Technically the row
+    r = int(row_column[1]) - 1 # Technically the column
     if r > 7 or r < 0: return "Bruh, Coord2Index(*row_column), first chara must be a-h only"
     if c > 7 or c < 0: return "Bruh, Coord2Index(row_*column), second chara must be 0 ≤ row ≤ 7"
     return r,c
 
+def ForwardThenBackward(x, y): # 2018 LeBron fr
+    _list = [0,1,2,3,4,5,6,7]
+    _list4 = []
+    _list2 = _list[:x]
+    _list3 = _list[x:]
+    _list3.remove(x)
+    _list4 = _list3 + _list2
 
-piece_dictionary = {0   : piece(" ",None,None)  ,   # Empty
-                    1   : piece("♟",True,1)    ,   # White Pawn
-                    2   : piece("♞",True,3)    ,   # White Knight
-                    3   : piece("♝",True,3)    ,   # White Bishop
-                    4   : piece("♜",True,5)    ,   # White Rook
-                    5   : piece("♛",True,9)    ,   # White Queen
-                    6   : piece("♚",True,None) ,   # White King
-                    7   : piece("♙",False,1)   ,   # Black Pawn
-                    8   : piece("♘",False,3)   ,   # Black Knight
-                    9   : piece("♗",False,3)   ,   # Black Bishop
-                    10  : piece("♖",False,5)   ,   # Black Rook
-                    11  : piece("♕",False,9)   ,   # Black Queen
-                    12  : piece("♔",False,None),   # Black King
+    _list2 = _list[:y]
+    _list3 = _list[y:]
+    _list3.remove(y)
+    _list4 += _list3 + _list2
+    return _list4
+    # download ram!! free
+    
+# def ReturnColumn(TwoD_list, column):
+#     _list = []
+#     for row in TwoD_list:
+#         _list.append(row[column])
+#     return _list
+
+piece_dictionary = {0   : piece(    " ",    None,   None,   "Empty"         ),
+                    1   : piece(    "♟",   True,   1,      "White Pawn"    ),
+                    2   : piece(    "♞",   True,   3,      "White Knight"  ),
+                    3   : piece(    "♝",   True,   3,      "White Bishop"  ),
+                    4   : piece(    "♜",   True,   5,      "White Rook"    ),
+                    5   : piece(    "♛",   True,   9,      "White Queen"   ),
+                    6   : piece(    "♚",   True,   None,   "White King"    ),
+                    7   : piece(    "♙",   False,  1,      "Black Pawn"    ),
+                    8   : piece(    "♘",   False,  3,      "Black Knight"  ),
+                    9   : piece(    "♗",   False,  3,      "Black Bishop"  ),
+                    10  : piece(    "♖",   False,  5,      "Black Rook"    ),
+                    11  : piece(    "♕",   False,  9,      "Black Queen"   ),
+                    12  : piece(    "♔",   False,  None,   "Black King"    ),
                     "P" : 1,
                     "N" : 2,
                     "B" : 3,
@@ -54,6 +76,19 @@ real_board = [
     [1,1,1,1,1,1,1,1],
     [4,2,3,5,6,3,2,4]
     ]
+
+# DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER
+real_board = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,2,0],
+    [4,0,0,0,1,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,4,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [4,0,0,0,0,5,0,0]
+    ]
+# DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER DELETE LATER
 
 real_board.reverse()
 
@@ -149,104 +184,95 @@ def check_Spot(destination, piece):
     else:
         return False
 
-def check_Pawn(destination, piece):
+def check_Spot_4_Any(destination):
     global real_board
-
-    # (Spot below, Two below)
-    found = (None, None)
-
-    if check_Spot(destination, piece) == False or real_board.index(real_board[destination[0]]) == 0:
-        return found
+    if real_board[destination[0]][destination[1]] != 0:
+        return True
     else:
-        if check_Spot((destination[0]-1,destination[1]), piece):
-            found = (destination[0]-1, destination[1])
-        elif check_Spot((destination[0]-2,destination[1]), piece) and real_board.index(real_board[destination[0]]) >= 2:
-            # Make sure we dont go out of bounds with that        and
-            found = (destination[0]-2, destination[1])
-    return found
+        return False
 
-def check_Rook(destination, piece):
+# Makes this so much cleaner now
+def return_Spot(destination):
+    global real_board
+    return real_board[destination[0]][destination[1]]
+
+# Redo later
+# def check_Pawn(destination, piece):
+#     global real_board
+
+#     # (Spot below, Two below)
+#     found = (None, None)
+
+#     if check_Spot(destination, piece) == False or real_board.index(real_board[destination[0]]) == 0:
+#         return found
+#     else:
+#         if check_Spot((destination[0]-1,destination[1]), piece):
+#             found = (destination[0]-1, destination[1])
+#         elif check_Spot((destination[0]-2,destination[1]), piece) and real_board.index(real_board[destination[0]]) >= 2:
+#             # Make sure we dont go out of bounds with that        and
+#             found = (destination[0]-2, destination[1])
+#     return found
+
+def check_Horizontally(destination, piece):
     global real_board
 
     # None  -   No rooks in the same row/column
     # False -   No more than 1 rooks in the same row/column
     # True  -   More than 1 in the same row/column
     #       (Same row relative to dest, Same column relative to dest)
-    found = (None, None)
+    found = 0
 
-    if check_Spot(destination, piece) == True:
-        #return found
-        print("Found", piece, "at", Index2Coord(destination))
-    # else:
-        # Loop to check possible rooks
-    # Loop columns first
-    for spots in range(7 - destination[1]):
-        if check_Spot((destination[0],destination[1]+spots+1),piece):
-            print("found on column:", chr(destination[1]+spots+98))
-            if found == (found[0],False):
-                found = (found[0],True)
-                print(found)
-                break
-            else: 
-                found = (found[0],False)
-    # Then rows
-    for spots in range(7 - destination[0]):
-        if check_Spot((destination[0]+spots+1,destination[1]),piece):
-            print("found on row:", destination[0]+spots+1)
-            if found == (False,None):
-                found = (True, None)
-                print(found)
-                break
-            else: 
-                found = (False, None)
+    if check_Spot_4_Any(destination):
+        print("Obstructed!")
+        print(f"Found piece: {piece_dictionary[return_Spot(destination)].fullName}, at", Index2Coord(destination))
+        return found
+    else:
+        list_to_go_over = ForwardThenBackward(destination[0],destination[1])
+        print(list_to_go_over)
+        # Check row horizontally, then column vertically.
 
-check_Rook((0,0),4)
+check_Horizontally((1,1),4)
 
 # These are useless for now
+# def checkColumn(piece, column, side_to_check = None):
+#     global real_board
+#     piece_to_check_for = piece
+#     # Just in case
+#     if side_to_check == None: # Check for current side's pieces
+#         piece_to_check_for = piece_dictionary[piece] + (move % 2 * 6)
+#     elif side_to_check == True: # Check for White pieces
+#         piece_to_check_for = piece_dictionary[piece]
+#     elif side_to_check == False: # Check for Black pieces
+#         piece_to_check_for = piece_dictionary[piece] + 6
+#     #
+#     for r in real_board:
+#         if r[column] == piece_to_check_for:
+#             print(r[column])
 
-def checkColumn(piece, column, side_to_check = None):
-    global real_board
-    piece_to_check_for = piece
-    # Just in case
-    if side_to_check == None: # Check for current side's pieces
-        piece_to_check_for = piece_dictionary[piece] + (move % 2 * 6)
-    elif side_to_check == True: # Check for White pieces
-        piece_to_check_for = piece_dictionary[piece]
-    elif side_to_check == False: # Check for Black pieces
-        piece_to_check_for = piece_dictionary[piece] + 6
-    #
-    for r in real_board:
-        if r[column] == piece_to_check_for:
-            print(r[column])
+# def checkRow(piece, row, side_to_check = None):
+#     global real_board
+#     piece_to_check_for = piece
+#     # Just in case
+#     if side_to_check == None: # Check for current side's pieces
+#         piece_to_check_for = piece_dictionary[piece] + (move % 2 * 6)
+#     elif side_to_check == True: # Check for White pieces
+#         piece_to_check_for = piece_dictionary[piece]
+#     elif side_to_check == False: # Check for Black pieces
+#         piece_to_check_for = piece_dictionary[piece] + 6
+#     #
+#     for r in real_board[7-row]:
+#         if r == piece:
+#             print(r)
 
-def checkRow(piece, row, side_to_check = None):
-    global real_board
-    piece_to_check_for = piece
-    # Just in case
-    if side_to_check == None: # Check for current side's pieces
-        piece_to_check_for = piece_dictionary[piece] + (move % 2 * 6)
-    elif side_to_check == True: # Check for White pieces
-        piece_to_check_for = piece_dictionary[piece]
-    elif side_to_check == False: # Check for Black pieces
-        piece_to_check_for = piece_dictionary[piece] + 6
-    #
-    for r in real_board[7-row]:
-        if r == piece:
-            print(r)
-
-
-# These are useless for now
 
 update_board()
 #interpreter(input())
-
 
 # Have checks for each piece.
 # These checks are checking destination availability first. (Wether or not capturing)
 # Then go backwards from destination to location. (Unless piece = Knight)
 # If no location given, trace every space in same pattern as piece's. 
 # (To check for more than one pieces that can make the same move provided)
-
 
 # If pieces found > 1, throw error.
 # Otherwise make move with the found piece. 
